a4af8a595a1ed4539f6f2181b2595649bb4c3986
Daniel Studart <danielstudart22@gmail.com>
2023-10-11 10:00:34 -0300

    atualização arrays 3

--- Arrays/codigo.js
+++ Arrays/codigo.js
@@ -1,14 +1,22 @@
 const ingrid = atletas [0];
 
 //window.onload = () => {   
-    const espaco_nome = document.getElementById('nome');
-    const imagem = document.querySelector('div#container > img');
-    const descricao = document.querySelector('div#container > p');
-
-    espaco_nome.innerText = ingrid.nome;
-    imagem.src = ingrid.imagem;
-    descricao.innerText = ingrid.descricao;
-
 //} (Não é necessário agora por causa do 'defer' no script no html)
+const espaco_nome = document.getElementById('nome');
+const imagem = document.querySelector('div#container > img');
+const descricao = document.querySelector('div#container > p');
+
+const preenche = (atleta) => {
+    espaco_nome.innerText = atleta.nome;
+    imagem.src = atleta.imagem;
+    descricao.innerText = atleta.descricao;
+
+}
+
+preenche(atletas[3]);
+
+
+
+
 
 
