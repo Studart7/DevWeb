08a155030a7843c5ecefccf0f327149cbb326bb5
Daniel Studart <danielstudart22@gmail.com>
2023-10-25 09:07:42 -0300

    Arrays-Layout

--- Arrays/codigo.js
+++ Arrays/codigo.js
@@ -2,6 +2,7 @@ const ingrid = atletas [0];
 
 //window.onload = () => {   
 //} (Não é necessário agora por causa do 'defer' no script no html)
+
 const espaco_nome = document.getElementById('nome');
 const imagem = document.querySelector('div#container > img');
 const descricao = document.querySelector('div#container > p');
@@ -10,7 +11,6 @@ const preenche = (atleta) => {
     espaco_nome.innerText = atleta.nome;
     imagem.src = atleta.imagem;
     descricao.innerText = atleta.descricao;
-
 }
 
 preenche(atletas[3]);
